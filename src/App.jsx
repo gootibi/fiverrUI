963013c577d0a3eaed085bc65a526eea9f92e5b4
gootibi <gootibi@gmail.com>
2023-12-14 20:05:29 +0100

    Component ad page create.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <div>
+      <Navbar />
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+      <h1>A történet szerint egy régi barátja meghal, ezért Foley visszatér Beverly Hills-be, hogy a helyi rendőrségen burjánzó korrupció hátterére fényt derítsen, nyomozásában pedig tőle eltávolodott, ügyvédként dolgozó lánya (Taylour Paige) és az ő expasija (Joseph Gordon-Levitt), egy detektív segítik.</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/messages/Messages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Messages.scss'
+
+function Messages() {
+  return (
+    <div>Messages</div>
+  )
+}
+
+export default Messages
